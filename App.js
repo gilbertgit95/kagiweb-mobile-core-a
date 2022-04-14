e0031fdcfd90d27bf482702dbb502f21bb4ff000
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-04-15 00:53:54 +0800

    update app icon

--- App.js
+++ App.js
@@ -71,8 +71,8 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.js</Text>
+            screen and then come back to see your.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
